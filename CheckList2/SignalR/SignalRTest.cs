a3364fc7ddc7ef4053be5ecc81365088d80993b0
a.gaeini <a.gaeini@AGHIL-PC>
2022-09-10 08:08:02 +0430

    Create test functions in client and server for test signalR

--- CheckList2/SignalR/SignalRTest.cs
+++ CheckList2/SignalR/SignalRTest.cs
@@ -1,8 +1,21 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace CheckList2.SignalR
 {
     public class SignalRTest : Hub
     {
+        public async Task SendTestMessage(string msg)
+        {
+            try
+            {
+                await Clients.All.SendAsync("RecieveRequestTest", msg);
+            }
+            catch (System.Exception ex)
+            {
+
+                throw;
+            }
+        }
     }
 }
