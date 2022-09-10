08f7ceb719399e81d2dada979ec11e1e9825277e
a.gaeini <a.gaeini@AGHIL-PC>
2022-09-10 08:18:06 +0430

    First connection and grouping client users

--- CheckList2/SignalR/SignalRTest.cs
+++ CheckList2/SignalR/SignalRTest.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CheckList2.SignalR
 {
     public class SignalRTest : Hub
     {
+        public static long clientsNumber = 0;
+
+        public override async Task OnConnectedAsync()
+        {
+            clientsNumber++;
+            if (clientsNumber % 2 == 0)
+                await Groups.AddToGroupAsync(Context.ConnectionId, "Even");
+            else
+                await Groups.AddToGroupAsync(Context.ConnectionId, "Odd");
+
+        }
+
         public async Task SendTestMessage(string msg)
         {
             try
